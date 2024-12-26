34f410b232bc2b9a0fe3065f9eac2be46a6fdba8
NancyBlack <NancyBlackguge@gmail.com>
2024-12-26 21:28:32 +0800

    first deploy!

---
+++ searchindex.en.js
@@ -0,0 +1,492 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Math",
+    "content": "2024-12-26 20:43:26\n公理之下，欧式不朽(_)",
+    "description": "欧氏几何够我看一辈子",
+    "tags": [
+      "Math",
+      "Matlab"
+    ],
+    "title": "Axiom",
+    "uri": "/math/axiom/index.html"
+  },
+  {
+    "breadcrumb": "Technology \u003e STM32H7",
+    "content": "2024-12-26 19:59:09\n看见的看不见的，反正没法说的bug",
+    "description": "反正你总能遇到",
+    "tags": [
+      "STM32"
+    ],
+    "title": "BUG",
+    "uri": "/technology/stm32h7/bug/index.html"
+  },
+  {
+    "breadcrumb": "Programming",
+    "content": "2024-12-26 20:13:11\n别说我不文明啊hugo的title有符号会自己产生bug，他识别的居然不是字符串就神奇以后有机会的话研究研究",
+    "description": "文明你我他",
+    "tags": [
+      "C++",
+      "VS"
+    ],
+    "title": "C艹",
+    "uri": "/programming/c\u0026#43;\u0026#43;/index.html"
+  },
+  {
+    "breadcrumb": "Life",
+    "content": "2024-12-26 20:53:51\n不一定好吃反正看着感兴趣的都扔进来。。。",
+    "description": "都是食谱啦",
+    "tags": [
+      "Food"
+    ],
+    "title": "Food",
+    "uri": "/life/food/index.html"
+  },
+  {
+    "breadcrumb": "Technology",
+    "content": "2024-12-26 19:51:44\n故事的起点，是野火的42JSF0AS-1000😏\n矢量控制这个六边形哪都能见到\nPid控制电机闭环啦",
+    "description": "电机它转呀转不停",
+    "tags": [
+      "PMSM"
+    ],
+    "title": "PMSM",
+    "uri": "/technology/pmsm/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "希望有天能手搓出一整套动力系统！干！\nPMSM电机它转呀转不停\nSTM32H7没有单片机电机怎么转",
+    "description": "发工资先换把焊枪！",
+    "tags": [],
+    "title": "Technology",
+    "uri": "/technology/index.html"
+  },
+  {
+    "breadcrumb": "Technology \u003e PMSM",
+    "content": "2024-12-26 19:59:09\nThis is a new page.",
+    "description": "这个六边形哪都能见到",
+    "tags": [
+      "PMSM",
+      "Simulink"
+    ],
+    "title": "矢量控制",
+    "uri": "/technology/pmsm/%E7%9F%A2%E9%87%8F%E6%8E%A7%E5%88%B6/index.html"
+  },
+  {
+    "breadcrumb": "Programming",
+    "content": "2024-12-26 20:13:16\n我觉得和C艹大差不差",
+    "description": "单片机跑不掉的",
+    "tags": [
+      "C",
+      "VS"
+    ],
+    "title": "C",
+    "uri": "/programming/c/index.html"
+  },
+  {
+    "breadcrumb": "Technology \u003e STM32H7",
+    "content": "2024-12-26 19:59:17\n先这么写着，等开始写再说",
+    "description": "都是从这开始的吧",
+    "tags": [
+      "STM32",
+      "GPIO"
+    ],
+    "title": "GPIO",
+    "uri": "/technology/stm32h7/gpio/index.html"
+  },
+  {
+    "breadcrumb": "Technology \u003e PMSM",
+    "content": "2024-12-26 19:59:17\nThis is a new page.",
+    "description": "电机闭环啦",
+    "tags": [
+      "PMSM",
+      "Simulink",
+      "Pid"
+    ],
+    "title": "Pid控制",
+    "uri": "/technology/pmsm/pid%E6%8E%A7%E5%88%B6/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "想搓出一个世界怎么能少得了代码\nC艹文明你我他\nC单片机跑不掉的\nMarkdown接触的第一种脚本语言\nLatex写论文真爽啊\nAuto-Hot-Key接触的第二种脚本语言",
+    "description": "能跑的bug就是好bug",
+    "tags": [],
+    "title": "Programming",
+    "uri": "/programming/index.html"
+  },
+  {
+    "breadcrumb": "Technology",
+    "content": "2024-12-26 19:51:44\n没错，故事的起点，还是野火😏\nBUG反正你总能遇到\nGPIO都是从这开始的吧",
+    "description": "没有单片机电机怎么转",
+    "tags": [
+      "STM32"
+    ],
+    "title": "STM32H7",
+    "uri": "/technology/stm32h7/index.html"
+  },
+  {
+    "breadcrumb": "Math",
+    "content": "2024-12-26 20:43:38\n总有那么多奇奇怪怪又可可爱爱~",
+    "description": "左看右看每条定理都不简单",
+    "tags": [
+      "Math",
+      "Matlab"
+    ],
+    "title": "Theorem",
+    "uri": "/math/theorem/index.html"
+  },
+  {
+    "breadcrumb": "Life",
+    "content": "2024-12-26 20:54:00\n啊也不知道能去几个地方",
+    "description": "逛吃逛吃逛吃逛吃~",
+    "tags": [],
+    "title": "Travel",
+    "uri": "/life/travel/index.html"
+  },
+  {
+    "breadcrumb": "Life",
+    "content": "2024-12-26 20:54:08\n看着高大上的都放进来~",
+    "description": "希望能看吧",
+    "tags": [],
+    "title": "Books",
+    "uri": "/life/books/index.html"
+  },
+  {
+    "breadcrumb": "Programming",
+    "content": "2024-12-26 20:13:27\nThis is a new page.",
+    "description": "接触的第一种脚本语言",
+    "tags": [
+      "Markdown",
+      "Obsidian"
+    ],
+    "title": "Markdown",
+    "uri": "/programming/markdown/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "哇这个世界永远这么奇妙！\nAxiom欧氏几何够我看一辈子\nTheorem左看右看每条定理都不简单\nTechniquesemmm也许这样算的快？\nHistory八卦谁不爱呢",
+    "description": "一生二，二生三，三生万物",
+    "tags": [],
+    "title": "Math",
+    "uri": "/math/index.html"
+  },
+  {
+    "breadcrumb": "Math",
+    "content": "2024-12-26 20:43:56\n你说这都睡研究的呢",
+    "description": "emmm也许这样算的快？",
+    "tags": [
+      "Math"
+    ],
+    "title": "Techniques",
+    "uri": "/math/techniques/index.html"
+  },
+  {
+    "breadcrumb": "Math",
+    "content": "2024-12-26 20:44:04\n有时间一定看！",
+    "description": "八卦谁不爱呢",
+    "tags": [
+      "Math"
+    ],
+    "title": "History",
+    "uri": "/math/history/index.html"
+  },
+  {
+    "breadcrumb": "Programming",
+    "content": "2024-12-26 20:13:33\nThis is a new page.",
+    "description": "写论文真爽啊",
+    "tags": [
+      "Latex",
+      "VsCode"
+    ],
+    "title": "Latex",
+    "uri": "/programming/latex/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "十万个为什么？\n暂时没法分类，多了以后也许声光电热力？🤣",
+    "description": "这个世界真奇秒",
+    "tags": [],
+    "title": "Science",
+    "uri": "/science/index.html"
+  },
+  {
+    "breadcrumb": "Programming",
+    "content": "2024-12-26 20:13:27\n你绝对猜不到我为什么会学这个东西[做过晋江审核吗？没错就是为了在win10桌面写一个自动点击器]",
+    "description": "接触的第二种脚本语言",
+    "tags": [
+      "AHK"
+    ],
+    "title": "Auto-Hot-Key",
+    "uri": "/programming/auto-hot-key/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "写点零零碎碎，看到的食谱[收藏吃灰]，想看的书[看着就高大上]，去过的地方[主要是吃]\nFood都是食谱啦\nTravel逛吃逛吃逛吃逛吃~\nBooks希望能看吧",
+    "description": "哎嘿就是浪",
+    "tags": [],
+    "title": "Life",
+    "uri": "/life/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "生死看淡不服就干！\n官方版：作为社会主义接班人，依法诚信纳税人，我们要学会用法律的武器保护自己，维护自身合法权益！",
+    "description": "生死看淡不服就干",
+    "tags": [],
+    "title": "Law",
+    "uri": "/law/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: AHK",
+    "uri": "/tags/ahk/index.html"
+  },
+  {
+    "breadcrumb": "Categories",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Category :: Book",
+    "uri": "/categories/book/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: C",
+    "uri": "/tags/c/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: C++",
+    "uri": "/tags/c\u0026#43;\u0026#43;/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "欢迎来到我的笔记，这是我第一通过Hugo尝试建立网站，主要用作笔记整理以及一些生活技巧，欢迎交流。\n【ps:评论功能还没学到，以后有机会再添加吧】\n【pps:链接功能也还没学到不然可以在这里放一下各个板块的链接】",
+    "description": "This is a website built by Feikun using Hugo.",
+    "tags": [],
+    "title": "Feikun's note",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Food",
+    "uri": "/tags/food/index.html"
+  },
+  {
+    "breadcrumb": "Categories",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Category :: Food",
+    "uri": "/categories/food/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "frontmatter",
+    "uri": "/frontmatter/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: GPIO",
+    "uri": "/tags/gpio/index.html"
+  },
+  {
+    "breadcrumb": "Categories",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Category :: History",
+    "uri": "/categories/history/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Latex",
+    "uri": "/tags/latex/index.html"
+  },
+  {
+    "breadcrumb": "Categories",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Category :: Law",
+    "uri": "/categories/law/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Markdown",
+    "uri": "/tags/markdown/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Math",
+    "uri": "/tags/math/index.html"
+  },
+  {
+    "breadcrumb": "Categories",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Category :: Math",
+    "uri": "/categories/math/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Matlab",
+    "uri": "/tags/matlab/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Obsidian",
+    "uri": "/tags/obsidian/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "options",
+    "uri": "/options/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Pid",
+    "uri": "/tags/pid/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: PMSM",
+    "uri": "/tags/pmsm/index.html"
+  },
+  {
+    "breadcrumb": "Categories",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Category :: Programming",
+    "uri": "/categories/programming/index.html"
+  },
+  {
+    "breadcrumb": "Categories",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Category :: Science",
+    "uri": "/categories/science/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Simulink",
+    "uri": "/tags/simulink/index.html"
+  },
+  {
+    "breadcrumb": "Categories",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Category :: Society",
+    "uri": "/categories/society/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: STM32",
+    "uri": "/tags/stm32/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  },
+  {
+    "breadcrumb": "Categories",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Category :: Technology",
+    "uri": "/categories/technology/index.html"
+  },
+  {
+    "breadcrumb": "Categories",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Category :: Travel",
+    "uri": "/categories/travel/index.html"
+  },
+  {
+    "breadcrumb": "Categories",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Category :: Tutorials",
+    "uri": "/categories/tutorials/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: VS",
+    "uri": "/tags/vs/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: VsCode",
+    "uri": "/tags/vscode/index.html"
+  }
+]
