9307768c35349e7b7423feeb17b11298429e6866
NancyBlack <NancyBlackguge@gmail.com>
2025-08-03 11:25:41 +0800

    Deploy updated site

---
+++ searchindex.zh-cn.js
@@ -0,0 +1,34 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "类别",
+    "uri": "/zh-cn/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "frontmatter",
+    "uri": "/zh-cn/frontmatter/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "options",
+    "uri": "/zh-cn/options/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "标签",
+    "uri": "/zh-cn/tags/index.html"
+  }
+]
