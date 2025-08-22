66c7514ad97b6d02f98d0d554f05d95480807847
NancyBlack <NancyBlackguge@gmail.com>
2025-08-22 20:32:55 +0800

    Update site

--- searchindex.en.js
+++ searchindex.en.js
@@ -71,7 +71,7 @@ var relearn_searchindex = [
   },
   {
     "breadcrumb": "Changelog",
-    "content": "2025-08-02 15:56:22\nVersion 0.0.0.241226 Hugo找的模板，部署在GitHub.io上，乱调一同总算有个样子，分了几个简单的板块，先跑起来再说。\nVersion 0.1.0.250801 终于有时间开始更新，这次添加了changelog模块记录一下网站的成长。\nVersion 0.1.1.250801 在主菜单栏显示当前版本。",
+    "content": "2025-08-02 15:56:22\nVersion 0.0.0.241226 Hugo找的模板，部署在GitHub.io上，乱调一同总算有个样子，分了几个简单的板块，先跑起来再说。\nVersion 0.1.0.250801 终于有时间开始更新，这次添加了changelog模块记录一下网站的成长。\nVersion 0.1.1.250801 在主菜单栏显示当前版本。\nVersion 0.2.0.250822 传输中断，网页崩溃，重新建立，增加备份",
     "description": "我的第一个网页",
     "tags": [],
     "title": "VERSION 0",
